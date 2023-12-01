56ddd07b98589149160b1bbe4dce392df6b83144
Peter Remez <remez.p85@gmail.com>
2023-12-01 15:09:09 +0100

    buttons, combination

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 import {useState} from "react";
-function App() {
-    const [activePlayer, setActivePlayer] = useState('X');
+import { WINNING_COMBINATIONS } from "./winning-combination";
 
-    function handleSelectSquare() {
-        setActivePlayer((curActivePlayer) => curActivePlayer === 'X'? 'O' : 'X');
+function App() {
+
+    const [gameTurns, setGameTurns] = useState([]);
+
+    function deriveActivePlayer(gameTurns) {
+        let currentPlayer = 'X';
+        if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+            currentPlayer = 'O';
+        }
+        return currentPlayer;
+    }
+
+    const activePlayer = deriveActivePlayer(gameTurns);
+
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        const currentPlayer = deriveActivePlayer(gameTurns);
+        setGameTurns(prevTurns => {
+            const updateTurns = [
+                {square: {row: rowIndex, col: colIndex}, player: currentPlayer},
+                ...prevTurns
+            ];
+            return updateTurns;
+        });
     }
     return (
     <main>
@@ -14,8 +36,12 @@ function App() {
           <Player name='Player 1' symbol='X' isActive={activePlayer === 'X'}/>
           <Player name='Player 2' symbol='0' isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard
+            onSelectSquare={handleSelectSquare}
+            turns={gameTurns}
+        />
       </div>
+        <Log turns={gameTurns}/>
     </main>
   )
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -5,23 +5,25 @@ const initialGameBoard = [
     [null, null, null],
     [null, null, null]
 ];
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
+    let gameBoard = initialGameBoard;
 
-    function handleSelectSquare(rowIndex, celIndex) {
-        setGameBoard((prevGameBoard)=> {
-            const newBoard = [...prevGameBoard.map(val => [...val])];
-            newBoard[rowIndex][celIndex] = activePlayerSymbol;
-            return newBoard;
-        });
-        onSelectSquare();
+    for( const turn of turns) {
+        const {square, player} = turn;
+        const {row, col} = square;
+        gameBoard[row][col] = player;
     }
     return (
         <ol id='game-board'>
             {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
-                {row.map((playerSymbol, celIndex) => <li key={celIndex} className='board-items'>
-                    <button onClick={() => handleSelectSquare(rowIndex, celIndex)}>{playerSymbol}</button>
-                </li>)}
+                {row.map((playerSymbol, colIndex) => <div key={colIndex} className='board-items'>
+                    <button
+                        onClick={() => onSelectSquare(rowIndex, colIndex)}
+                        disabled={playerSymbol !== null}
+                    >
+                        {playerSymbol}
+                    </button>
+                </div>)}
             </li>)}
         </ol>
     );
