97f0ad78efc022bdfe38a0e622f9a385aab4a287
Peter Remez <remez.p85@gmail.com>
2023-12-02 12:42:53 +0100

    all logic

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,21 +1,11 @@
 import {useState} from "react";
 
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-];
-export default function GameBoard({ onSelectSquare, turns }) {
-    let gameBoard = initialGameBoard;
 
-    for( const turn of turns) {
-        const {square, player} = turn;
-        const {row, col} = square;
-        gameBoard[row][col] = player;
-    }
+export default function GameBoard({ onSelectSquare, board }) {
+
     return (
         <ol id='game-board'>
-            {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
+            {board.map((row, rowIndex) => <li key={rowIndex}>
                 {row.map((playerSymbol, colIndex) => <div key={colIndex} className='board-items'>
                     <button
                         onClick={() => onSelectSquare(rowIndex, colIndex)}
